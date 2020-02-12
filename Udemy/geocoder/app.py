deb90460af3a9687c7b8d17f53a4e6645b324ecd
Lyasinkovska <l.yasinkovska@gmail.com>
2020-02-12 22:39:51 +0200

    changed geocoder files

--- Udemy/geocoder/app.py
+++ Udemy/geocoder/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+# app = Flask(__name__) # to make the app run without any
+app = Flask(__name__,)
 
 
-@app.route("/")
+@app.route("/354")
 def home():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
